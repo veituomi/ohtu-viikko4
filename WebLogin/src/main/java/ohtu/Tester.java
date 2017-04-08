36a45c2c6a2a8c20f3ff064f5ec0b5d26e444916
Veini Tuominen <veini.tuominen@hotmail.com>
2017-04-08 15:04:55 +0300

    6. Selenium, eli web-selaimen simulointi ohjelmakoodista

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -4,32 +4,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-
+        WebDriver driver = new HtmlUnitDriver();
         driver.get("http://localhost:4567");
-        
-        sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
-        sleep(2);
+        System.out.println(driver.getPageSource());
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("akke");
         element = driver.findElement(By.name("login"));
-        
-        sleep(2);
         element.submit();
 
-        sleep(3);
-        
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekk");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekk");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("kepp");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("kepp");
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        System.out.println(driver.getPageSource());
+
         driver.quit();
     }
     
